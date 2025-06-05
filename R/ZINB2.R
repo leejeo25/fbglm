ee7a31bca63f9b8ead3e008f5f593dca963476d2
Jeonghwa Lee <41245766+leejeo25@users.noreply.github.com>
2025-06-05 11:15:23 -0400

    Update ZINB2.R

--- R/ZINB2.R
+++ R/ZINB2.R
@@ -20,47 +20,90 @@
 #' @export
 #'
 #' @examples
+#' Example 1
 #' library(agridat)
-#' library(bbmle)
 #' sample<-sample(270, 30)
 #' my_y<-ridout.appleshoots$roots[sample]
 #' my_x<-data.frame(pho=ridout.appleshoots$pho[sample])
 #' ZINB2(y=my_y, x=my_x  )
-#'
-ZINB2<-function(y,x){
+#' Example 2
+#'ZINB2(y=c(0,1,4,2,2,1,3,0,0,6,4), x=data.frame(x1=c(1,3,4,5,4,3,6,7,4,3,1 )))
+#' Example 3
+#'ZINB2(y=c(0,1,4,2,2,1,3,0,0,6,4), x=data.frame(x1=c(1,3,4,5,4,3,6,7,4,3,1 )), w=c(1,1,1,1,1,1,1,1,1,1,1))
+
+ZINB2<-function(y,x,w=NULL){
+  if(!is.null(w)){ 
   w_0<-which(y==0)
   dnb<-function(k,theta,mu){prob<-theta/(mu+theta);dnbinom(x=k, size=theta, prob=prob, log = FALSE)}
-
+  
   data3<-cbind(intercept=rep(1, length(y)),x)
   nn<-dim(data3)[2]
   nn0<-nn+1
   nn10<-2*nn
   nn1<-2*nn+1
   nn2<-3*nn
-
+  
   Theta<-paste0(colnames( data3),"(theta)")
   Mu<-paste0(colnames( data3),"(mu)")
   Pi<-paste0(colnames( data3),"(pi)")
-
-  likelihood_nb<-function( X ){
+  
+  likelihood_nb<-function( X){
     theta<-exp(rowSums( mapply(`*`,data3,X[1:nn]) ))
     mu<-exp(rowSums( mapply(`*`,data3,X[nn0:nn10])   ))
     pi<-1/(1+exp(-(rowSums( mapply(`*`,data3,X[nn1:nn2]) ) )))
     DAT<-rbind(theta, mu)
     DAT2<-rbind(y,theta, mu)[,-w_0]
+    
+    -sum(w[w_0]*log(pi[w_0]+(1-pi[w_0])* apply(DAT[, w_0]  ,2,
+                                               function(x){dnb(0,x[1],x[2] )} )))-
+      sum(w[-w_0]*log((1- pi[-w_0])* apply(DAT2  ,2,
+                                           function(x){dnb(x[1],x[2],x[3] )} )  ))
+  }
+  
+  bbmle::parnames(likelihood_nb) <- c(Theta, Mu, Pi)
+  
+  est<-bbmle::mle2(
+    minuslogl = likelihood_nb ,
+    start = setNames(rep(0, nn2),c(Theta, Mu, Pi ) ),
+    vecpar = TRUE)
+  my_list <- list( "AIC"=AIC(est),"Log-likelihood"= -est@details$value, "coef" = est@coef, "summary" = bbmle::summary(est))
+  return(my_list)}  
+  else{  w_0<-which(y==0)
+  dnb<-function(k,theta,mu){prob<-theta/(mu+theta);dnbinom(x=k, size=theta, prob=prob, log = FALSE)}
+  
+  data3<-cbind(intercept=rep(1, length(y)),x)
+  nn<-dim(data3)[2]
+  nn0<-nn+1
+  nn10<-2*nn
+  nn1<-2*nn+1
+  nn2<-3*nn
+  
+  Theta<-paste0(colnames( data3),"(theta)")
+  Mu<-paste0(colnames( data3),"(mu)")
+  Pi<-paste0(colnames( data3),"(pi)")
+  
+  likelihood_nb<-function( X){
+    theta<-exp(rowSums( mapply(`*`,data3,X[1:nn]) ))
+    mu<-exp(rowSums( mapply(`*`,data3,X[nn0:nn10])   ))
+    pi<-1/(1+exp(-(rowSums( mapply(`*`,data3,X[nn1:nn2]) ) )))
+    DAT<-rbind(theta, mu)
+    DAT2<-rbind(y,theta, mu)[,-w_0]
+    
     -sum(log(pi[w_0]+(1-pi[w_0])* apply(DAT[, w_0]  ,2,
                                         function(x){dnb(0,x[1],x[2] )} )))-
       sum(log((1- pi[-w_0])* apply(DAT2  ,2,
                                    function(x){dnb(x[1],x[2],x[3] )} )  ))
   }
-
+  
   bbmle::parnames(likelihood_nb) <- c(Theta, Mu, Pi)
-
+  
   est<-bbmle::mle2(
     minuslogl = likelihood_nb ,
     start = setNames(rep(0, nn2),c(Theta, Mu, Pi ) ),
     vecpar = TRUE)
-  my_list <- list( "Log-likelihood"= -est@details$value, "coef" = est@coef, "summary" = summary(est))
-  return(my_list)  }
+  my_list <- list( "AIC"=AIC(est),"Log-likelihood"= -est@details$value, "coef" = est@coef, "summary" = bbmle::summary(est))
+  return(my_list)}
+  
+}
 
 
