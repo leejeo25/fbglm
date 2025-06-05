853901c8c3e924854146d1436c5bb111a689d129
Jeonghwa Lee <41245766+leejeo25@users.noreply.github.com>
2025-06-05 10:59:19 -0400

    Add files via upload

---
+++ R/ZIB.R
@@ -0,0 +1,70 @@
+
+ZIB<-function(y,x,w=NULL){
+  if(!is.null(w)){
+  w_0<-which(y==0)
+n<-max(y)
+
+data3<-cbind(intercept=rep(1, length(y)),x)
+nn<-dim(data3)[2]
+nn0<-nn+1
+nn10<-2*nn
+
+P<-paste0(colnames( data3),"(P)")
+Pi<-paste0(colnames( data3),"(pi)")
+
+likelihood_b<-function( X){
+  P<-1/(1+exp(-(rowSums( mapply(`*`,data3,X[1:nn]) ) )))
+  pi<-1/(1+exp(-(rowSums( mapply(`*`,data3,X[nn0:nn10]) ) )))
+  
+  DAT2<-rbind(y,P)[,-w_0]
+  
+  -sum(w[w_0]*log(pi[w_0]+(1-pi[w_0])*dbinom(0,n,P[w_0] )))-
+    sum(w[-w_0]*log((1- pi[-w_0])* apply(DAT2  ,2,
+                                         function(x){dbinom(x[1],n,x[2] )} )  ))
+}
+
+bbmle::parnames(likelihood_b) <- c(P, Pi)
+
+est<-bbmle::mle2(
+  minuslogl = likelihood_b ,
+  start = setNames(rep(0, nn10),c(P, Pi ) ),
+  vecpar = TRUE)
+my_list <- list( "AIC"=AIC(est),"Log-likelihood"= -est@details$value, "coef" = est@coef, "summary" = bbmle::summary(est))
+return(my_list) }
+  else{ w_0<-which(y==0)
+  n<-max(y)
+  
+  data3<-cbind(intercept=rep(1, length(y)),x)
+  nn<-dim(data3)[2]
+  nn0<-nn+1
+  nn10<-2*nn
+  
+  P<-paste0(colnames( data3),"(P)")
+  Pi<-paste0(colnames( data3),"(pi)")
+  
+  likelihood_b<-function( X){
+    P<-1/(1+exp(-(rowSums( mapply(`*`,data3,X[1:nn]) ) )))
+    pi<-1/(1+exp(-(rowSums( mapply(`*`,data3,X[nn0:nn10]) ) )))
+    
+    DAT2<-rbind(y,P)[,-w_0]
+    
+    -sum(log(pi[w_0]+(1-pi[w_0])*dbinom(0,n,P[w_0] )))-
+      sum(log((1- pi[-w_0])* apply(DAT2  ,2,
+                                   function(x){dbinom(x[1],n,x[2] )} )  ))
+  }
+  
+  bbmle::parnames(likelihood_b) <- c(P, Pi)
+  
+  est<-bbmle::mle2(
+    minuslogl = likelihood_b ,
+    start = setNames(rep(0, nn10),c(P, Pi ) ),
+    vecpar = TRUE)
+  my_list <- list( "AIC"=AIC(est),"Log-likelihood"= -est@details$value, "coef" = est@coef, "summary" = bbmle::summary(est))
+  return(my_list) 
+  }
+  
+}
+
+ZIB(y=c(0,1,4,2,2,1,3,0,0,6,4), x=data.frame(x1=c(1,3,4,5,4,3,6,7,4,3,1 )))
+
+ZIB(y=c(0,1,4,2,2,1,3,0,0,6,4), x=data.frame(x1=c(1,3,4,5,4,3,6,7,4,3,1 )), w=c(1,1,1,1,1,1,1,1,1,1,1))
